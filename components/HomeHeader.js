915a0327c9162011fcc0e36df96431ea9ab8b04d
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-08-30 17:01:44 +0530

    refactor: Optimize HomeHeader component and update styles

---
+++ components/HomeHeader.js
@@ -0,0 +1,36 @@
+import { View, Image } from "react-native";
+import { CommonStyles, HomeScreen } from "../styles/style";
+import CustomText from "../components/CustomText/CustomText";
+import CustomButton from "../components/Buttons/ButtonComponent";
+
+export const HomeHeader = () => {
+  return (
+    <View style={[CommonStyles.header, HomeScreen.header]}>
+      <View style={HomeScreen.headerContainer}>
+        <Image
+          style={HomeScreen.userImage}
+          source={require("../assets/images/user.png")} // Adjust the path as necessary
+        />
+        <View style={HomeScreen.headerContent}>
+          <CustomText style={HomeScreen.userName}>Michael Mitc</CustomText>
+          <CustomText style={HomeScreen.userDesignation}>
+            Lead UI/UX Designer
+          </CustomText>
+        </View>
+      </View>
+      <View style={HomeScreen.headerIcon}>
+        <CustomButton
+          color="neutral50"
+          outlined={true}
+          padding={0}
+          // onPress={() => navigation.navigate("Notifications")}
+          iconName="notifications-outline"
+          iconSize={24}
+          iconColor="#000"
+          style={HomeScreen.notificationButton}
+        />
+      </View>
+    </View>
+  );
+};
+export default HomeHeader;
