e786011c1505c08125e274faba1d146eea6171aa
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-09-06 12:02:23 +0530

    refactor: Remove unused import in babel.config.js and package.json

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@components": "./components",
-            "@screens": "./screens",
-            "@utils": "./utils",
-            "@styles": "./styles",
-          },
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
