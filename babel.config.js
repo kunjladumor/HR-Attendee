33c3823de07314cad492474893c658899578f00a
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-09-09 13:56:22 +0530

    Refactor babel.config.js to add module-resolver plugin and react-native-dotenv module, Created path variables for better routing.

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@navigation": "./src/navigation",
+            "@utils": "./src/utils",
+            "@styles": "./src/styles",
+            "@assets": "./assets",
+          },
+        },
+      ],
+      ["module:react-native-dotenv"],
+    ],
   };
 };
