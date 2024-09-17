ca22e19ec9b1fc0323aead258653f5a7c8fc0777
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-09-17 16:19:19 +0530

    Refactor color styles to support dark theme

--- src/styles/ColorStyles.js
+++ src/styles/ColorStyles.js
@@ -1,9 +1,7 @@
 // colors.js
+import { Appearance } from "react-native";
 
-// Always use the primary color as the main color
-// Always use Hex values for colors
-
-const colors = {
+const colorsLight = {
   // Primary and secondary colors
 
   primary: "#3185ff", // Blue
@@ -11,16 +9,16 @@ const colors = {
   primary2: "#A3D139", // Leaf Green
   secondary2: "#30BEB6", // Teal Green
 
-  // Text Color
-  text: "#333333", // Dark Gray
-  text2: "#4F4F4F", // Gray
-  text3: "#828282", // Light Gray
-
   // Additional shades
   lightPrimary: "#B3D4FF", // Light Blue
   darkPrimary: "#005BBB", // Dark Blue
   lightSecondary: "#FFCDD2", // Light Red
   darkSecondary: "#D32F2F", // Dark Red
+
+  // Text colors
+  text: "#242124",
+  text2: "#515151",
+  text3: "#9E9E9E",
 
   // Neutral colors
   white: "#FFFFFF",
@@ -46,5 +44,51 @@ const colors = {
   surface: "#FFFFFF", // Surface (cards, modals, etc.)
   overlay: "rgba(0, 0, 0, 0.5)", // Overlay for modals or loaders
 };
+
+// Dark Theme Colors
+const colorsDark = {
+  // Primary and secondary colors
+
+  primary: "#3185ff", // Blue
+  secondary: "#F44336", // Red
+  primary2: "#A3D139", // Leaf Green
+  secondary2: "#30BEB6", // Teal Green
+
+  // Additional shades
+  lightPrimary: "#B3D4FF", // Light Blue
+  darkPrimary: "#005BBB", // Dark Blue
+  lightSecondary: "#FFCDD2", // Light Red
+  darkSecondary: "#D32F2F", // Dark Red
+
+  text: "#FFFFFF", // White text for better contrast
+  text2: "#E0E0E0", // Light Gray
+  text3: "#BDBDBD", // Lighter Gray
+
+  white: "#E0E0E0", // Light Gray for backgrounds
+  black: "#121212", // Dark Black
+
+  neutral10: "#3B3B3B",
+  neutral20: "#515151",
+  neutral30: "#626262",
+  neutral40: "#7E7E7E",
+  neutral50: "#9E9E9E",
+  neutral60: "#B1B1B1",
+  neutral70: "#CFCFCF",
+  neutral80: "#E1E1E1",
+  neutral90: "#F7F7F7",
+
+  success: "#4CAF50", // Green
+  danger: "#F44336", // Red
+  warning: "#FF9800", // Orange
+  info: "#64B5F6", // Lighter Blue
+
+  background: "#121212", // Dark Gray Background
+  surface: "#1E1E1E", // Dark Surface
+  overlay: "rgba(255, 255, 255, 0.5)", // White overlay for contrast
+};
+
+// Function to return the appropriate colors based on system preference
+const colors =
+  Appearance.getColorScheme() === "dark" ? colorsDark : colorsLight;
 
 export default colors;
