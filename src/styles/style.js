5581dde97cbd98183148623d6cde7bb6545a3b69
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-09-19 12:54:35 +0530

    Refactor shadow styles in CommonStyles

--- src/styles/style.js
+++ src/styles/style.js
@@ -57,16 +57,7 @@ export const CommonStyles = StyleSheet.create({
     paddingTop: /* statusBarHeight + 20 */ 0,
     paddingBottom: 90,
   },
-  shadow: {
-    shadowColor: "#7F5DF0",
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
+
   centerIcon: {
     justifyContent: "center",
     alignItems: "center",
