e9feedef5662b751d457c50042a7ff56a43cc2cb
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-09-21 13:26:33 +0530

    Refactor package.json and app.config.js files to load alternate splash screen when available.

---
+++ app.config.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+const alternativeSplashImage = "./assets/alternative-splash.jpg";
+const defaultSplashImage = "./assets/splash.png";
+
+const splashImage = fs.existsSync(alternativeSplashImage)
+  ? alternativeSplashImage
+  : defaultSplashImage;
+
+export default {
+  expo: {
+    name: "hr-attendance-app",
+    slug: "hr-attendance-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: splashImage,
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: splashImage,
+      resizeMode: "cover",
+      //   backgroundColor: colors.background,
+    },
+
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          "We need your location to show your position on the map",
+        NSLocationAlwaysUsageDescription:
+          "We need your location to show your position on the map",
+      },
+    },
+    android: {
+      package: "com.cyberax.hrattendee",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        // backgroundColor: colors.background,
+      },
+      permissions: ["ACCESS_FINE_LOCATION", "ACCESS_COARSE_LOCATION"],
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "bd3c1bf4-25d4-4699-a448-575f86970927",
+      },
+    },
+  },
+};
