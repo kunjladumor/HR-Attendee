f7ec787fa07aa6ed51d7b4c140b77721da44b704
Kunj Ladumor <ladumorkunj1601@gmail.com>
2024-10-10 16:32:01 +0530

    Refactor NotificationScreen component to include tab navigation for different types of notifications

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, StyleSheet, Dimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import NotificationCard from "@components/NotificationCard"; // Adjust the import based on your project structure
 import CustomText from "@components/CustomText"; // Adjust the import based on your project structure
 import { CommonStyles } from "@styles/style"; // Adjust the import based on your project structure
@@ -14,6 +15,7 @@ const notifications = [
     content: "You have received a new message.",
     time: "10:30 AM",
     favourite: true,
+    type: "message",
   },
   {
     id: "2",
@@ -22,6 +24,7 @@ const notifications = [
     content: "Your system requires an update.",
     time: "9:15 AM",
     favourite: true,
+    type: "alert",
   },
   {
     id: "3",
@@ -30,6 +33,7 @@ const notifications = [
     content: "You have received a new message.",
     time: "Yesterday",
     favourite: true,
+    type: "message",
   },
   {
     id: "4",
@@ -38,6 +42,7 @@ const notifications = [
     content: "Your system requires an update.",
     time: "Yesterday",
     favourite: true,
+    type: "alert",
   },
   {
     id: "5",
@@ -46,6 +51,7 @@ const notifications = [
     content: "You have received a new message.",
     time: "Yesterday",
     favourite: true,
+    type: "message",
   },
   {
     id: "6",
@@ -54,6 +60,7 @@ const notifications = [
     content: "Your system requires an update.",
     time: "Yesterday",
     favourite: true,
+    type: "alert",
   },
   {
     id: "7",
@@ -62,6 +69,7 @@ const notifications = [
     content: "You have received a new message.",
     time: "Yesterday",
     favourite: true,
+    type: "message",
   },
   {
     id: "8",
@@ -70,6 +78,7 @@ const notifications = [
     content: "Your system requires an update.",
     time: "Yesterday",
     favourite: true,
+    type: "alert",
   },
   {
     id: "9",
@@ -78,6 +87,7 @@ const notifications = [
     content: "You have received a new message.",
     time: "Yesterday",
     favourite: true,
+    type: "message",
   },
   {
     id: "10",
@@ -86,6 +96,7 @@ const notifications = [
     content: "Your system requires an update.",
     time: "Yesterday",
     favourite: true,
+    type: "alert",
   },
   {
     id: "11",
@@ -94,17 +105,84 @@ const notifications = [
     content: "You have received a new message.",
     time: "Yesterday",
     favourite: true,
+    type: "message",
   },
 ];
 
 const NotificationScreen = ({ navigation }) => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "all", title: "All" },
+    { key: "message", title: "Messages" },
+    { key: "alert", title: "Alerts" },
+  ]);
+
+  const renderScene = SceneMap({
+    all: () => (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          {notifications.map((notification) => (
+            <NotificationCard
+              key={notification.id}
+              icon={notification.icon}
+              title={notification.title}
+              content={notification.content}
+              time={notification.time}
+              navigation={navigation}
+              favourite={notification.favourite}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    ),
+    message: () => (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          {notifications
+            .filter((notification) => notification.type === "message")
+            .map((notification) => (
+              <NotificationCard
+                key={notification.id}
+                icon={notification.icon}
+                title={notification.title}
+                content={notification.content}
+                time={notification.time}
+                navigation={navigation}
+                favourite={notification.favourite}
+              />
+            ))}
+        </View>
+      </ScrollView>
+    ),
+    alert: () => (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          {notifications
+            .filter((notification) => notification.type === "alert")
+            .map((notification) => (
+              <NotificationCard
+                key={notification.id}
+                icon={notification.icon}
+                title={notification.title}
+                content={notification.content}
+                time={notification.time}
+                navigation={navigation}
+                favourite={notification.favourite}
+              />
+            ))}
+        </View>
+      </ScrollView>
+    ),
+  });
+
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <View
         style={[
-          CommonStyles.header,
+          CommonStyles.row,
           {
             paddingHorizontal: 20,
+            gap: 10,
           },
         ]}
       >
@@ -116,19 +194,20 @@ const NotificationScreen = ({ navigation }) => {
         />
         <CustomText style={CommonStyles.header}>Notifications</CustomText>
       </View>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        {notifications.map((notification) => (
-          <NotificationCard
-            key={notification.id}
-            icon={notification.icon}
-            title={notification.title}
-            content={notification.content}
-            time={notification.time}
-            navigation={navigation}
-            favourite={notification.favourite}
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: Dimensions.get("window").width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            indicatorStyle={{ backgroundColor: colors.primary }}
+            style={{ backgroundColor: colors.background }}
+            labelStyle={{ color: colors.text }}
           />
-        ))}
-      </ScrollView>
+        )}
+      />
     </View>
   );
 };
@@ -137,10 +216,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+    padding: 20,
     paddingTop: 0,
+    paddingBottom: 90,
   },
   scrollViewContent: {
-    paddingHorizontal: 20,
     paddingBottom: 20,
   },
 });
